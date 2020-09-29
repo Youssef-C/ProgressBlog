cb0d1f79f2170c76f23a12cb2d74de664e6ba4c6
youssef.c <youssef.chardi@student.kyh.se>
2020-09-29 17:09:40 +0200

    began implementation of a form validator in contact.js

---
+++ contact/contactInput.js
@@ -0,0 +1,16 @@
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event)
+{
+    if (email.validity.typeMismatch)
+        {
+            email.setCustomValidity("I am expecting an e-mail Address!");
+        }
+            else    
+                {
+                    email.setCustomValidity("");
+                }
+
+});
+
+
